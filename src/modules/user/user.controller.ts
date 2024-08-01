e5071bb46604d7c0a7a0d623aa570fd092791131
Alifoxy <nightfoxy666@gmail.com>
2024-08-01 21:04:07 +0300

    first commit, created modules

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreateCarDto, UpdateCatDto, ListAllEntities } from './dto';
+
+@Controller('cars')
+export class CarsController {
+  @Post()
+  create(@Body() createCarDto: CreateCarDto) {
+    return 'This action adds a new car';
+  }
+
+  @Get()
+  findAll(@Query() query: ListAllEntities) {
+    return `This action returns all cars (limit: ${query.limit} items)`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() createCarDto: CreateCarDto) {
+    return `This action updates a #${id} car`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} car`;
+  }
+}
